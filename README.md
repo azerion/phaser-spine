17b6349e647356e71615bf8346c64a3746a9f61f
AleBles <a.bles@orangegames.com>
2016-01-19 23:29:38 +0100

    Some more updates ;)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 Phaser Spine
 ============
+
+Getting Started
+---------------
+
+Usage
+-----
+
+Changelog
+---------
+### 1.0.0
+* Initial release
+
+Disclaimer
+----------
+We at OrangeGames just love playing and creating awesome games. We aren't affiliated with Phaser.io and/or esotericsoftware. We just needed some awesome spine animations in our awesome HTML5 games. Feel free to use it for enhancing your own awesome games!
--- ts/Spine.ts
+++ ts/Spine.ts
@@ -1,93 +1,307 @@
 module Fabrique {
-    export module Plugins {
-        export interface SpineObjectFactory extends Phaser.GameObjectFactory {
-            spine: (x: number, y: number, key: string, group?: Phaser.Group) => Fabrique.Spine;
-        }
+    export class Spine extends Phaser.Group
+    {
+        private spineData: any;
+        public skeleton: {
+            setToSetupPose: () => void;
+            updateWorldTransform: () => void;
+            drawOrder: any[];
+            slots: any[];
+        };
+        private stateData: any;
+        private state: {
+            update: (dt: number) => void;
+            apply: (skeleton: any) => void;
+            setAnimationByName: (trackIndex: number, animationName: string, loop: boolean) => void;
+            addAnimationByName: (trackIndex: number, animationName: string, loop: boolean, delay: number) => void;
+            setMixByName: (fromName: string, toName: string, duration: number) => void;
+        };
+        private slotContainers: any[];
+        private lastTime: number;
 
-        export interface SpineCache extends Phaser.Cache {
-            addSpine: (key: string, data: any) => void;
-            getSpine: (key: string) => any;
-            spine: {[key: string]: any};
-        }
+        public game: Fabrique.Plugins.SpineGame;
 
-        export interface SpineLoader extends Phaser.Loader {
-            spine: (key: string, url: string) => void;
-            cache: SpineCache;
-        }
+        /**
+         * @class Spine
+         * @extends Phaser.Group
+         * @constructor
+         * @param game {Phaser.Game} the game reference to add this object
+         * @param key {String} the key to find the assets for this object
+         */
+        constructor(game: Fabrique.Plugins.SpineGame, key: string)
+        {
+            super(game);
 
-        export interface SpineGame extends Phaser.Game {
-            add: SpineObjectFactory;
-            load: SpineLoader;
-            phaserSpine: Spine;
-            cache: SpineCache;
-        }
+            var data = this.game.cache.getSpine(key);
 
-        export class Spine extends Phaser.Plugin {
+            var textureLoader = new Fabrique.SpineTextureLoader(data.basePath, false);
+            // create a spine atlas using the loaded text and a spine texture loader instance //
+            var spineAtlas = new spine.Atlas(game.cache.getText(data.atlas), textureLoader);
+            // now we use an atlas attachment loader //
+            var attachmentLoader = new spine.AtlasAttachmentLoader(spineAtlas);
+            // spine animation
+            var spineJsonParser = new spine.SkeletonJson(attachmentLoader);
 
-            constructor(game: SpineGame, parent: PIXI.DisplayObject) {
-                super(game, parent);
+            //get the Skeleton Data
+            this.spineData = spineJsonParser.readSkeletonData(game.cache.getJSON(key));
 
-                this.addSpineCache();
-                this.addSpineFactory();
-                this.addSpineLoader();
+            if (!this.spineData) {
+                throw new Error('Spine data must be preloaded using Loader.spine');
             }
 
-            private addSpineLoader() {
-                (<Fabrique.Plugins.SpineLoader>Phaser.Loader.prototype).spine = function(key: string, url: string) {
+            this.skeleton = new spine.Skeleton(this.spineData);
+            this.skeleton.updateWorldTransform();
 
-                    var atlasKey = key+"Atlas";
+            this.stateData = new spine.AnimationStateData(this.spineData);
+            this.state = new spine.AnimationState(this.stateData);
 
-                    var cacheData = {
-                        atlas: atlasKey,
-                        basePath: url.substring(0, url.lastIndexOf('/'))
-                    };
+            this.slotContainers = [];
 
-                    this.json(key, url);
-                    this.text(atlasKey, url.substr(0, url.lastIndexOf('.')) + '.atlas');
-                    this.image(key, url.substr(0, url.lastIndexOf('.')) + '.png');
+            for (var i = 0, n = this.skeleton.drawOrder.length; i < n; i++) {
+                var slot = this.skeleton.drawOrder[i];
+                var attachment = slot.attachment;
+                var slotContainer = new Phaser.Group(game);
+                this.slotContainers.push(slotContainer);
+                this.add(slotContainer);
 
-                    this.game.cache.addSpine(key, cacheData);
-                };
+                if (attachment instanceof spine.RegionAttachment) {
+                    var spriteName = attachment.rendererObject.name;
+                    var sprite = this.createSprite(slot, attachment);
+                    slot.currentSprite = sprite;
+                    slot.currentSpriteName = spriteName;
+                    slotContainer.add(sprite);
+                } else if (attachment instanceof spine.MeshAttachment) {
+                    var mesh = this.createMesh(slot, attachment);
+                    slot.currentMesh = mesh;
+                    slot.currentMeshName = attachment.name;
+                    slotContainer.add(mesh);
+                }
             }
 
-            /**
-             * Extends the GameObjectFactory prototype with the support of adding spine. this allows us to add spine methods to the game just like any other object:
-             * game.add.spine();
-             */
-            private addSpineFactory() {
-                (<Fabrique.Plugins.SpineObjectFactory>Phaser.GameObjectFactory.prototype).spine = function(x: number, y: number, key: string, group?: Phaser.Group): Fabrique.Spine
-                {
-                    if (group === undefined) { group = this.world; }
-
-                    var spineObject = new Fabrique.Spine(this.game, key);
-
-                    spineObject.skeleton.setToSetupPose();
-                    spineObject.position.x = x;
-                    spineObject.position.y = y;
-
-                    return group.add(spineObject);
-                };
-            }
-
-            /**
-             * Extends the Phaser.Cache prototype with spine properties
-             */
-            private addSpineCache(): void {
-                //Create the cache space
-                (<Fabrique.Plugins.SpineCache>Phaser.Cache.prototype).spine = {};
-
-                //Method for adding a spine dict to the cache space
-                (<Fabrique.Plugins.SpineCache>Phaser.Cache.prototype).addSpine = function(key: string, data: any)
-                {
-                    this.spine[key] = data;
-                };
-
-                //Method for fetching a spine dict from the cache space
-                (<Fabrique.Plugins.SpineCache>Phaser.Cache.prototype).getSpine = function(key: string): any
-                {
-                    return this.spine[key];
-                };
-            }
+            this.autoUpdate = true;
         }
+
+        get autoUpdate(): boolean {
+            return (this.updateTransform === Fabrique.Spine.prototype.autoUpdateTransform);
+        };
+
+        set autoUpdate(value: boolean) {
+            this.updateTransform = value ? Fabrique.Spine.prototype.autoUpdateTransform : PIXI.DisplayObjectContainer.prototype.updateTransform;
+        };
+
+
+        /**
+         * Update the spine skeleton and its animations by delta time (dt)
+         *
+         * @method update
+         * @param dt {Number} Delta time. Time by which the animation should be updated
+         */
+        public update(dt?: number) {
+            if (dt === undefined) {
+                return;
+            }
+
+            this.state.update(dt);
+            this.state.apply(this.skeleton);
+            this.skeleton.updateWorldTransform();
+
+            var drawOrder = this.skeleton.drawOrder;
+            for (var i = 0, n = drawOrder.length; i < n; i++) {
+
+                var slot = this.skeleton.drawOrder[i];
+                var attachment = slot.attachment;
+                var slotContainer = this.slotContainers[i];
+
+                if (!attachment) {
+                    slotContainer.visible = false;
+                    continue;
+                }
+
+                var type = attachment.type;
+
+                if (type === spine.AttachmentType.region) {
+                    if (attachment.rendererObject) {
+                        if (!slot.currentSpriteName || slot.currentSpriteName !== attachment.name) {
+                            var spriteName = attachment.rendererObject.name;
+                            if (slot.currentSprite !== undefined) {
+                                slot.currentSprite.visible = false;
+                            }
+                            slot.sprites = slot.sprites || {};
+                            if (slot.sprites[spriteName] !== undefined) {
+                                slot.sprites[spriteName].visible = true;
+                            } else {
+                                var sprite = this.createSprite(slot, attachment);
+                                slotContainer.add(sprite);
+                            }
+                            slot.currentSprite = slot.sprites[spriteName];
+                            slot.currentSpriteName = spriteName;
+                        }
+                    }
+
+                    var bone = slot.bone;
+
+                    slotContainer.position.x = bone.worldX + attachment.x * bone.m00 + attachment.y * bone.m01;
+                    slotContainer.position.y = bone.worldY + attachment.x * bone.m10 + attachment.y * bone.m11;
+                    slotContainer.scale.x = bone.worldScaleX;
+                    slotContainer.scale.y = bone.worldScaleY;
+
+                    slotContainer.rotation = -(slot.bone.worldRotation * spine.degRad);
+
+                    //slot.currentSprite.tint = PIXI.rgb2hex([slot.r,slot.g,slot.b]);
+                } else if (type === spine.AttachmentType.skinnedmesh) {
+                    if (!slot.currentMeshName || slot.currentMeshName !== attachment.name) {
+                        var meshName = attachment.name;
+                        if (slot.currentMesh !== undefined) {
+                            slot.currentMesh.visible = false;
+                        }
+
+                        slot.meshes = slot.meshes || {};
+
+                        if (slot.meshes[meshName] !== undefined) {
+                            slot.meshes[meshName].visible = true;
+                        } else {
+                            var mesh = this.createMesh(slot, attachment);
+                            slotContainer.add(mesh);
+                        }
+
+                        slot.currentMesh = slot.meshes[meshName];
+                        slot.currentMeshName = meshName;
+                    }
+
+                    attachment.computeWorldVertices(slot.bone.skeleton.x, slot.bone.skeleton.y, slot, slot.currentMesh.vertices);
+
+                } else {
+                    slotContainer.visible = false;
+                    continue;
+                }
+                slotContainer.visible = true;
+
+                slotContainer.alpha = slot.a;
+            }
+        };
+
+        /**
+         * When autoupdate is set to yes this function is used as pixi's updateTransform function
+         *
+         * @method autoUpdateTransform
+         * @private
+         */
+        public autoUpdateTransform() {
+            this.lastTime = this.lastTime || Date.now();
+            var timeDelta = (Date.now() - this.lastTime) * 0.001;
+            this.lastTime = Date.now();
+
+            this.update(timeDelta);
+
+            PIXI.DisplayObjectContainer.prototype.updateTransform.call(this);
+        };
+
+        /**
+         * Create a new sprite to be used with spine.RegionAttachment
+         *
+         * @method createSprite
+         * @param slot {spine.Slot} The slot to which the attachment is parented
+         * @param attachment {spine.RegionAttachment} The attachment that the sprite will represent
+         * @private
+         */
+        public createSprite(slot: any, attachment: any) {
+
+            var descriptor = attachment.rendererObject;
+            var baseTexture = descriptor.page.rendererObject;
+            var spriteRect = new PIXI.Rectangle(descriptor.x,
+                descriptor.y,
+                descriptor.rotate ? descriptor.height : descriptor.width,
+                descriptor.rotate ? descriptor.width : descriptor.height);
+
+            var spriteTexture = new PIXI.Texture(baseTexture, spriteRect);
+            var sprite = new Phaser.Sprite(this.game, 0, 0, spriteTexture);
+
+            var baseRotation = descriptor.rotate ? Math.PI * 0.5 : 0.0;
+            sprite.scale.set(descriptor.width / descriptor.originalWidth, descriptor.height / descriptor.originalHeight);
+            sprite.rotation = baseRotation - (attachment.rotation * spine.degRad);
+            sprite.anchor.x = sprite.anchor.y = 0.5;
+
+            slot.sprites = slot.sprites || {};
+            slot.sprites[descriptor.name] = sprite;
+
+            //set bitmap for tint
+            this.createBitMap(sprite);
+
+            return sprite;
+        };
+
+        public createBitMap(sprite: Phaser.Sprite) {
+            var bitmap = this.game.make.bitmapData(sprite.width, sprite.height);
+
+            var spriteData = {
+                scaleX: sprite.scale.x,
+                scaleY: sprite.scale.y,
+                rotation: sprite.rotation
+            };
+
+            //reset the rotation and scale of sprite to draw into the bitmap
+            sprite.scale.x = sprite.scale.y = 1;
+            sprite.rotation = 0;
+
+            //draw the sprite into the bitmap
+            bitmap.draw(sprite, sprite.width/2, sprite.height/2);
+            bitmap.update();
+
+            //resotre the scale and rotation of the sprite
+            sprite.scale.set(spriteData.scaleX, spriteData.scaleY);
+            sprite.rotation = spriteData.rotation;
+
+            sprite.loadTexture(bitmap);
+        }
+
+        public createMesh(slot: any, attachment: any) {
+            var descriptor = attachment.rendererObject;
+            var baseTexture = descriptor.page.rendererObject;
+            var texture = new PIXI.Texture(baseTexture);
+
+            var strip = new PIXI.Strip(texture);
+            //strip.drawMode = PIXI.Strip.DrawModes.TRIANGLES;
+            strip.canvasPadding = 1.5;
+
+            strip.vertices = [];
+            strip.uvs = attachment.uvs;
+            strip.indices = attachment.triangles;
+
+            slot.meshes = slot.meshes || {};
+            slot.meshes[attachment.name] = strip;
+
+            return strip;
+        };
+
+        /**
+         * [setMixByName wrap to stateData.setMixByName]
+         * @param {String} fromName [source animation name]
+         * @param {String} toName   [target animation name]
+         * @param {Float} duration [Duration in the transition of the animations]
+         */
+        public setMixByName(fromName: string, toName: string, duration: number) {
+            this.stateData.setMixByName(fromName, toName, duration);
+        };
+
+        /**
+         * [setAnimationByName set the animation for the specified track]
+         * @param {Integer} trackIndex    [index to find the animation track]
+         * @param {String} animationName [the name of the aniamtion to set]
+         * @param {Boolean} loop          [true if the animation must continue in a loop]
+         */
+        public setAnimationByName(trackIndex: number, animationName: string, loop: boolean) {
+            this.state.setAnimationByName(trackIndex, animationName, loop);
+        };
+
+        /**
+         * [addAnimationByName description]
+         * @param {[type]} trackIndex    [description]
+         * @param {[type]} animationName [description]
+         * @param {[type]} loop          [description]
+         * @param {[type]} delay         [description]
+         */
+        public addAnimationByName(trackIndex: number, animationName: string, loop: boolean, delay: number) {
+            this.state.addAnimationByName(trackIndex, animationName, loop, delay);
+        };
     }
 }
